df599c52d3a14a326db4bcf4a833577cf93bde43
chueh <b10902076@csie.ntu.edu.tw>
2024-05-11 00:13:50 +0800

    change layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,6 +139,22 @@ export default function Page() {
           })}
         </Section>
         <Section>
+          <h2 className="text-xl font-bold">技能</h2>
+          <div className="flex flex-wrap gap-1">
+            {RESUME_DATA.skills.map((skill) => {
+              return <Badge key={skill}>{skill}</Badge>;
+            })}
+          </div>
+        </Section>
+        <Section>
+          <h2 className="text-xl font-bold">語文能力</h2>
+          <div className="flex flex-wrap gap-1">
+            {RESUME_DATA.languages.map((language) => {
+              return <Badge key={language}>{language}</Badge>;
+            })}
+          </div>
+        </Section>
+        <Section>
           <h2 className="text-xl font-bold">社團經歷</h2>
           {RESUME_DATA.work.map((work) => {
             return (
@@ -182,23 +198,7 @@ export default function Page() {
             );
           })}
         </Section>
-        <Section>
-          <h2 className="text-xl font-bold">技能</h2>
-          <div className="flex flex-wrap gap-1">
-            {RESUME_DATA.skills.map((skill) => {
-              return <Badge key={skill}>{skill}</Badge>;
-            })}
-          </div>
-        </Section>
-        <Section>
-          <h2 className="text-xl font-bold">語文能力</h2>
-          <div className="flex flex-wrap gap-1">
-            {RESUME_DATA.languages.map((language) => {
-              return <Badge key={language}>{language}</Badge>;
-            })}
-          </div>
-        </Section>
-
+        
         {/* <Section className="print-force-new-page scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
