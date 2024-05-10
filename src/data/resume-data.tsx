0e9801a8a494e6b2d25a75525c23c3fbe94cfb0a
chueh <b10902076@csie.ntu.edu.tw>
2024-05-10 13:22:11 +0800

    add photo v2

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -11,7 +11,7 @@ export const RESUME_DATA = {
     "Student at National Taiwan University majored in Computer Science and Information Engineering",
   summary:
     "",
-  avatarUrl: "https://drive.google.com/file/d/1HvL3xa-zVRw1trabBnRojHI_qp8TC89x/view?usp=sharing",
+  avatarUrl: "https://drive.google.com/a/g.ntu.edu.tw/thumbnail?id=1VbKRC073t_dflOUn22imdybCLDtJYK3E",
 //   personalWebsiteUrl: "",
   contact: {
     email: "b10902076@csie.ntu.edu.tw",
